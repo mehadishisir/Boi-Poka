fcde4306d1843f9307d7ae5e6ed23e07fd4dd793
mehadi <mehadishisir@gmail.com>
2025-10-30 21:48:46 +0600

    add navbar and footer

--- src/assets/Routes/Routes.jsx
+++ src/assets/Routes/Routes.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { createBrowserRouter } from "react-router";
 import Root from "../root/Root";
+import ErrorPage from "./Error/ErrorPage";
+import Home from "../pages/Home/Home";
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home,
+      },
+    ],
   },
 ]);
